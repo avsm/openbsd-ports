36036b3056591d036be713d3f172d1a0446813a3
branch-fixup <branch-fixup>
2000-07-02 19:40:30 +0000

    Add files from parent branch HEAD: 	net/epic4/patches/patch-Makefile.in 	net/epic4/patches/patch-include_config.h

---
+++ net/epic4/patches/patch-include_config.h
@@ -0,0 +1,11 @@
+--- include/config.h.orig	Thu Dec 30 13:30:54 1999
++++ include/config.h	Sat Jan 29 22:25:55 2000
+@@ -516,7 +516,7 @@
+ #undef EPIC_DEBUG		/* force coredump on panic */
+ #define EXEC_COMMAND		/* allow /exec comamnd */
+ #undef HACKED_DCC_WARNING	/* warn if handshake != sender */
+-#undef HARD_UNFLASH		/* do a hard reset instead of soft on refresh */
++#define HARD_UNFLASH		/* do a hard reset instead of soft on refresh */
+ #undef NO_BOTS			/* no bots allowed */
+ #undef NO_CHEATING		/* always do it the "right" way, no shortcuts */
+ #undef STRIP_EXTRANEOUS_SPACES	/* strip leading and trailing spaces */

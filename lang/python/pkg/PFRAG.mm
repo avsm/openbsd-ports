395575f599dee501eabf5f59e67a488d21f66100
Marco S Hyman <marc@openbsd.org>
2000-09-15 04:55:55 +0000

    bring lang as of cut-over date into 2.7 branch

--- lang/python/pkg/PFRAG.mm
+++ lang/python/pkg/PFRAG.mm
@@ -1,3 +1,4 @@
+@comment $OpenBSD: PFRAG.mm,v 1.1.2.1 2000/09/15 04:55:41 marc Exp $
 lib/python1.5/lib-dynload/audioop.so
 lib/python1.5/lib-dynload/imageop.so
 lib/python1.5/lib-dynload/rgbimgmodule.so

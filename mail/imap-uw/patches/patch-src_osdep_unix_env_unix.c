196251ad3b3a5790d6b162ff840e84dfd2949e28
Jakob Schlyter <jakob@openbsd.org>
2002-12-09 20:26:26 +0000

    update to imap release 2002a.DEV.SNAP-0212051126 aka 2002.234, fixes post-login buffer overflow.

--- mail/imap-uw/patches/patch-src_osdep_unix_env_unix.c
+++ mail/imap-uw/patches/patch-src_osdep_unix_env_unix.c
@@ -1,8 +1,23 @@
-$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.1 2001/09/24 22:01:49 jakob Exp $
+$OpenBSD: patch-src_osdep_unix_env_unix.c,v 1.1.4.1 2002/12/09 20:26:26 jakob Exp $
 
---- src/osdep/unix/env_unix.c.orig	Thu Dec 21 01:12:13 2000
-+++ src/osdep/unix/env_unix.c	Thu Jan 18 16:11:09 2001
-@@ -971,7 +971,8 @@ long dotlock_lock (char *file,DOTLOCK *b
+--- src/osdep/unix/env_unix.c.orig	Tue Sep  3 08:39:10 2002
++++ src/osdep/unix/env_unix.c	Sun Sep  8 14:55:34 2002
+@@ -773,14 +773,12 @@ char *myhomedir ()
+ static char *mymailboxdir ()
+ {
+   char *home = myhomedir ();
+-  if (!myMailboxDir && home) {	/* initialize if first time */
+     if (mailsubdir) {
+       char tmp[MAILTMPLEN];
+       sprintf (tmp,"%s/%s",home,mailsubdir);
+       myMailboxDir = cpystr (tmp);/* use pre-defined subdirectory of home */
+     }
+     else myMailboxDir = cpystr (home);
+-  }
+   return myMailboxDir ? myMailboxDir : "";
+ }
+ 
+@@ -1036,7 +1034,8 @@ long dotlock_lock (char *file,DOTLOCK *b
        }
        close (pi[0]); close (pi[1]);
      }
